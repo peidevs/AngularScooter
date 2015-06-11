55a2b516cd8e445c25fb2fcf226a983e3e90e95a
Sean Whalley <swhalley@gmail.com>
2015-06-11 17:56:00 -0300

    Added some baseleine tests to get started with jasmine

--- js/service/config.js
+++ js/service/config.js
@@ -2,7 +2,7 @@
 scooter.factory( 'config', function(){
     this.theme = 'scooter';
 
-    this.themes = [
+    this.themes = [ //There is such a thing as .value (angular type like factory) to store constants like this
         { name: 'Scooter', path: 'scooter'},
         { name: 'PEIDevs', path: 'pei'},
         { name: 'Sticky Note', path: 'postit' }
---
+++ tests/spec/service/attendees.js
@@ -0,0 +1,37 @@
+describe( "attendees", function(){
+    var attendees;
+    var httpBackend;
+    var users = [{ name: 'foo'}, {name: 'bar'}];
+
+    beforeEach( module('scooter'));
+
+    beforeEach( inject( function( $injector ){
+        attendees = $injector.get( 'attendees' );
+        httpBackend = $injector.get('$httpBackend');
+    }));
+
+    beforeEach( function(){
+       httpBackend.when( 'GET', 'attendees.json').respond(users);
+    });
+
+    describe("get()", function(){
+
+
+        it("returns a list of attendees", function(){
+            httpBackend.flush();
+
+            var actual = attendees.get();
+            expect( actual).toBeTruthy();
+            expect( actual.length ).toBe(2);
+            expect( actual[0].name).toBe(users[0].name);
+            expect( actual[1].name).toBe(users[1].name);
+        });
+    });
+
+    describe("reset()", function(){
+
+        it("sets all policies back to alive", function(){
+
+        });
+    });
+});
