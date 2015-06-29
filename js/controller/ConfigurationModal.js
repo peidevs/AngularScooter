e85ae0174fe1c71c7f9212eab9bd218983269905
Sean Whalley <swhalley@gmail.com>
2015-06-28 21:13:05 -0300

    Ability to move user from dead to alive and back again

--- js/controller/ConfigurationModal.js
+++ js/controller/ConfigurationModal.js
@@ -1,18 +1,32 @@
 'use strict';
 scooter.controller( 'ConfigurationModal', function($scope, $modalInstance, config, attendees){
     $scope.themes = config.themes;
-    $scope.selected = angular.copy( config.theme );
+    $scope.selectedTheme = angular.copy( config.theme );
 
     $scope.players = attendees.get();
+    $scope.selectedAlivePlayers = [];
+    $scope.selectedDeadPlayers = [];
 
     $scope.cancel = function(){
         $modalInstance.dismiss();
     };
 
     $scope.save = function(){
-        config.theme = $scope.selected;
-        $modalInstance.close( $scope.selected );
+        config.theme = $scope.selectedTheme;
+        $modalInstance.close( $scope.selectedTheme );
 
-    }
+    };
+
+    $scope.killPlayer = function(){
+        $scope.selectedAlivePlayers.forEach(function( player ){
+           player.isAlive = false;
+        });
+    };
+
+    $scope.resurrectPlayer = function(){
+        $scope.selectedDeadPlayers.forEach(function( player ){
+            player.isAlive = true;
+        })
+    };
 
 });
