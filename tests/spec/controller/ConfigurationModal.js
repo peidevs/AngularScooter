003b92ab1bfaf75dbb357f39070396a24de66efe
Sean Whalley <swhalley@gmail.com>
2015-09-17 18:10:24 -0300

    a broken attenpt to add Configuration Modal/Controller Tests

--- tests/spec/controller/ConfigurationModal.js
+++ tests/spec/controller/ConfigurationModal.js
@@ -1,3 +1,35 @@
-describe( 'ConfigurationModal', function(){
+describe('ConfigurationModal', function () {
+    beforeEach(module('scooter'));
 
+    var $controller;
+    var $modalInstance;
+
+    beforeEach(inject(function (_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    describe('$scope.addPlayer', function () {
+        var $scope;
+        var controller;
+
+        beforeEach(function(){
+            $scope = {};
+            controller = $controller( 'ConfigurationModal', {$scope: $scope});
+        });
+
+        it('Nothing entered by the user, the player is not added to the game', function () {
+            $scope.playerToAdd = '';
+
+            controller.addPlayer();
+            expect($scope.players).toEqual('strong');
+        });
+
+        it('A user\'s name was entered. The player is added to the game', function(){
+
+        } );
+
+        it('Resets the playerToAdd box after adding the player', function(){
+
+        });
+    });
 });
