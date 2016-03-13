94e2bf3c32cd5b636b8569bbba4824de72816003
Ross Evans <therealrossevans@gmail.com>
2016-03-13 15:14:03 -0300

    Add default profile picture when using Add Player GUI

--- js/model/Player.js
+++ js/model/Player.js
@@ -1,7 +1,7 @@
 'use strict'
 function Player( name, photo_link ){
     this.name = name;
-    this.photo_link = photo_link;
+    this.photo_link = photo_link || "http://img2.meetupstatic.com/img/458386242735519287330/noPhoto_50.png";
     this.isAlive = true;
     this.isWinner = false;
 };
