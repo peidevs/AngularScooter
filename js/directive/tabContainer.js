36ead60a727dba317920ab790957a3fc642635f5
Sean Whalley <swhalley@gmail.com>
2016-03-31 22:29:00 -0300

    tabs converted from controller to directive

---
+++ js/directive/tabContainer.js
@@ -0,0 +1,45 @@
+'use strict';
+/**
+ *  Code copied and modified from Angular tutorial
+ *  https://docs.angularjs.org/guide/directive
+ */
+scooter.directive( "tabContainer", function(){
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'views/partial/_tabContainer.html',
+        scope: {},
+        controller: ['$scope', function($scope) {
+            var panes = $scope.panes = [];
+
+            $scope.select = function(pane) {
+                angular.forEach(panes, function(pane) {
+                    pane.selected = false;
+                });
+                pane.selected = true;
+            };
+
+            this.addPane = function(pane) {
+                if (panes.length === 0) {
+                    $scope.select(pane);
+                }
+                panes.push(pane);
+            };
+        }]
+    };
+} );
+
+scooter.directive( "tab", function(){
+    return {
+        require: '^^tabContainer',
+        restrict: 'E',
+        scope: {
+            name : '@',
+            template : '@'
+        },
+        templateUrl: 'views/partial/_tab.html',
+        link: function(scope, element, attrs, parentController) {
+            parentController.addPane(scope);
+        }
+    };
+});
